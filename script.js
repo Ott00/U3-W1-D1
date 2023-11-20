8534b6af0c17917b29bd0ac66580579a2b2ef22c
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-11-20 14:21:44 +0100

    changed how I log results

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 var randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
-console.log(randomNumber);
+console.log("Numero Casuale:", randomNumber);
+var generateRandomNumber = function () {
+    return Math.floor(Math.random() * (100 - 1) + 1);
+};
+var checkWinner = function () {
+    var guess1 = generateRandomNumber();
+    console.log("Giocatore 1:", guess1);
+    var guess2 = generateRandomNumber();
+    console.log("Giocatore 2:", guess2);
+    var absoluteDifference1 = Math.abs(randomNumber - guess1);
+    var absoluteDifference2 = Math.abs(randomNumber - guess2);
+    randomNumber === guess1
+        ? console.log("Il giocatore 1 ha indovinato il numero")
+        : randomNumber === guess2
+            ? console.log("Il giocatore 2 ha indovinato il numero")
+            : absoluteDifference1 < absoluteDifference2
+                ? console.log("Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!")
+                : absoluteDifference1 > absoluteDifference2
+                    ? console.log("Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!")
+                    : console.log("Nessuno dei due ha azzeccato il numero casuale, entrambi i giocatori si sono avvicinati in egual modo");
+};
+checkWinner();
 // const checkWinner: any = (guess1: number, guess2: number) => {
 //   if (randomNumber === guess1) {
 //     console.log("Il giocatore 1 ha indovinato il numero");
@@ -23,18 +44,3 @@ console.log(randomNumber);
 //     }
 //   }
 // };
-var checkWinner = function (guess1, guess2) {
-    var absoluteDifference1 = Math.abs(randomNumber - guess1);
-    var absoluteDifference2 = Math.abs(randomNumber - guess2);
-    console.log("Giocatore 1: " + guess1 + "; " + "Giocatore 2: " + guess2);
-    randomNumber === guess1
-        ? console.log("Il giocatore 1 ha indovinato il numero")
-        : randomNumber === guess2
-            ? console.log("Il giocatore 2 ha indovinato il numero")
-            : absoluteDifference1 < absoluteDifference2
-                ? console.log("Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!")
-                : absoluteDifference1 > absoluteDifference2
-                    ? console.log("Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!")
-                    : console.log("Nessuno dei due ha azzeccato il numero casuale, entrambi i giocatori si sono avvicinati in egual modo");
-};
-checkWinner(13, 78);
--- script.ts
+++ script.ts
@@ -1,5 +1,37 @@
 const randomNumber: number = Math.floor(Math.random() * (100 - 1) + 1);
-console.log(randomNumber);
+console.log("Numero Casuale:", randomNumber);
+
+const generateRandomNumber = function (): number {
+  return Math.floor(Math.random() * (100 - 1) + 1);
+};
+
+const checkWinner = function (): void {
+  const guess1: number = generateRandomNumber();
+  console.log("Giocatore 1:", guess1);
+  const guess2: number = generateRandomNumber();
+  console.log("Giocatore 2:", guess2);
+
+  const absoluteDifference1: number = Math.abs(randomNumber - guess1);
+  const absoluteDifference2: number = Math.abs(randomNumber - guess2);
+
+  randomNumber === guess1
+    ? console.log("Il giocatore 1 ha indovinato il numero")
+    : randomNumber === guess2
+    ? console.log("Il giocatore 2 ha indovinato il numero")
+    : absoluteDifference1 < absoluteDifference2
+    ? console.log(
+        "Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!"
+      )
+    : absoluteDifference1 > absoluteDifference2
+    ? console.log(
+        "Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!"
+      )
+    : console.log(
+        "Nessuno dei due ha azzeccato il numero casuale, entrambi i giocatori si sono avvicinati in egual modo"
+      );
+};
+
+checkWinner();
 
 // const checkWinner: any = (guess1: number, guess2: number) => {
 //   if (randomNumber === guess1) {
@@ -25,27 +57,3 @@ console.log(randomNumber);
 //     }
 //   }
 // };
-
-const checkWinner: any = (guess1: number, guess2: number) => {
-  const absoluteDifference1: number = Math.abs(randomNumber - guess1);
-  const absoluteDifference2: number = Math.abs(randomNumber - guess2);
-
-  console.log("Giocatore 1: " + guess1 + "; " + "Giocatore 2: " + guess2);
-  randomNumber === guess1
-    ? console.log("Il giocatore 1 ha indovinato il numero")
-    : randomNumber === guess2
-    ? console.log("Il giocatore 2 ha indovinato il numero")
-    : absoluteDifference1 < absoluteDifference2
-    ? console.log(
-        "Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!"
-      )
-    : absoluteDifference1 > absoluteDifference2
-    ? console.log(
-        "Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!"
-      )
-    : console.log(
-        "Nessuno dei due ha azzeccato il numero casuale, entrambi i giocatori si sono avvicinati in egual modo"
-      );
-};
-
-checkWinner(13, 78);
